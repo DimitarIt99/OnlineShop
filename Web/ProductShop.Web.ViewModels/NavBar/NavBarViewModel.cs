cce843ff3635aa1240a2781b8d77952cdd83ae2c
Dimitar <DimitarIt99@users.noreply.github.com>
2020-04-04 13:24:51 +0300

    Created a NavBar view component and render it in my layout

---
+++ Web/ProductShop.Web.ViewModels/NavBar/NavBarViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductShop.Web.ViewModels.NavBar
+{
+    using System.Collections.Generic;
+
+    using ProductShop.Web.ViewModels.Category;
+
+    public class NavBarViewModel
+    {
+        public NavBarViewModel()
+        {
+            this.Categories = new HashSet<CategoryNameViewModel>();
+        }
+
+        public string Privacy { get; set; }
+
+        public string AddProduct { get; set; }
+
+        public string Category { get; set; }
+
+        public IEnumerable<CategoryNameViewModel> Categories { get; set; }
+    }
+}
---
+++ Web/ProductShop.Web/ViewComponents/NavBarViewComponent.cs
@@ -0,0 +1,31 @@
+﻿namespace ProductShop.Web.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using ProductShop.Services.Data;
+    using ProductShop.Web.ViewModels.NavBar;
+
+    public class NavBarViewComponent : ViewComponent
+    {
+        private readonly ICategoriesService service;
+
+        public NavBarViewComponent(ICategoriesService service)
+        {
+            this.service = service;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = this.service.AllCategoryNames();
+            var navBarItems = new NavBarViewModel
+            {
+                Privacy = "Privacy",
+                AddProduct = "Add a Product",
+                Category = "Categories",
+                Categories = categories,
+            };
+
+            return this.View(navBarItems);
+        }
+    }
+}
