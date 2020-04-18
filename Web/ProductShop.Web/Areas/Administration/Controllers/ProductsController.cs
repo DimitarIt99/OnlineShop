b3d541772544b5f0c134bf169036ad09e1353762
Dimitar <DimitarIt99@users.noreply.github.com>
2020-04-18 12:02:28 +0300

    Created the admin controller and views for products.

---
+++ Web/ProductShop.Web/Areas/Administration/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿namespace ProductShop.Web.Areas.Administration.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using ProductShop.Services.Data;
+    using ProductShop.Web.ViewModels.Products;
+
+    public class ProductsController : AdministrationController
+    {
+        private readonly IProductsService service;
+
+        public ProductsController(IProductsService service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Edit(int id)
+        {
+            // service
+            var productToEdit = this.service.GetProductForEditById(id);
+
+            return this.View(productToEdit);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditProductViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            await this.service.EditProductAsync(model);
+            return this.RedirectToAction("Details", "Products", new { area = string.Empty, id = model.Id });
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await this.service.RemoveProductAsync(id);
+
+            return this.RedirectToAction("Index", "Home", new { area = string.Empty });
+        }
+    }
+}
